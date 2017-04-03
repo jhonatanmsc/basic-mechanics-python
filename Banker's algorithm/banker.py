e440931bc5c681cce49fa776d74f40d746f27b1c
matheusJhonatan <azulcodecademy@gmail.com>
2017-04-03 09:00:14 -0300

    pequenas atualizacoes

--- Banker's algorithm/banker.py
+++ Banker's algorithm/banker.py
@@ -4,7 +4,7 @@ def main():
 	continuar = True
 
 	print("###Digite a quantidade dos recursos A,B e C com um espaco entre os numeros###")
-	recursos_disponiveis			= input("Quanto de cada recurso o sistema tem disponível: ")
+	recursos_disponiveis = input("Quanto de cada recurso o sistema tem disponível: ")
 	recursos_disponiveis = recursos_disponiveis.split(" ")
 	recursos_disponiveis = [int(element) for element in recursos_disponiveis]
 
@@ -20,7 +20,7 @@ def main():
 		recursos_max_processo = [int(element) for element in recursos_max_processo]
 		recursos_atuais_processo = [int(element) for element in recursos_atuais_processo]
 
-		#criacao de um processo...
+		#criacao de um objeto so pra manipular...
 		processo = Processo(recursos_max_processo, recursos_atuais_processo)
 
 		#colocando em uma lista...
@@ -32,37 +32,36 @@ def main():
 	for i in range(len(processos)-1):
 		print(Processo.to_string(processos[i]))
 
-	achou = False
 	while(len(processos) > 0):
-		for it in range(len(processos)):
-			estado = []
-			indice = 0
+		for i in range(len(processos)):
+			deadlock = 0
 			print("\n---			informacoes do processo: ")
 			print("Recurso A do processo: ", processos[it]._max_recursos[0])
 			print("Recurso B do processo: ", processos[it]._max_recursos[1])
 			print("Recurso C do processo: ", processos[it]._max_recursos[2])
 
-			for i in range(3):
-				atuais = processos[it]._atuais_recursos[i] 				#recursos atuais do processo
-				max_processo_recursos = processos[it]._max_recursos[i] 	#maximo derecursos que o processo precisa
-				disponivel = recursos_disponiveis[i]					#recursos disponiveis
-				if(max_processo_recursos - atuais <= disponivel):
-					indice = i
-					recursos_disponiveis[i] = disponivel + atuais
-					processos.pop(it)
-					achou = True
-			estado.append(achou)
+			for j in range(len(processos._max_recursos)):
+				atuais = processos[i]._atuais_recursos[j] 				#recursos atuais do processo
+				max_processo_recursos = processos[i]._max_recursos[j] 	#maximo derecursos que o processo precisa
+				disponivel = recursos_disponiveis[j]					#recursos disponiveis
+				if(not(max_processo_recursos - atuais <= disponivel)):
+					deadlock += 1
+					break
+
+			if(deadlock == 0):
+				for d in range(len(recursos_disponiveis)):
+					recursos_disponiveis[d] += processos[j]._atuais_recursos[d]
+
 
 			print("---			Recursos disponiveis")
 			print("Recurso A: ", recursos_disponiveis[0])
 			print("Recurso B: ", recursos_disponiveis[1])
 			print("Recurso C: ", recursos_disponiveis[2])
 
-		if(not achou):
-			print("estado nao seguro")
+		if(achou):
+			processos.pop(it)
 			break
-		elif(len(processos) == 0):
-			print("estado seguro")
+		else:
 
 			
 
